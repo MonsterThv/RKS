81de46e12e9fac87616515d684c19629c572d275
Sergey <s9262406004@gmail.com>
2024-10-05 11:38:12 +0300

    RK1

---
+++ RK1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    float sum = 0;
+    for (int i = 1; i <= 10; i++) {
+        float a = 1 / sqrt(i * i * i);
+        sum += a;
+        cout << a << endl;
+    }
+    cout << sum;
+}
